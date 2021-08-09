fd19fbd6ee31231c0bf44e5df676cbc5e6eb56d3
davidgraycs <88401701+davidgraycs@users.noreply.github.com>
2021-08-08 21:11:15 -0500

    Add files via upload

--- main.c
+++ main.c
@@ -1,16 +1,33 @@
 #include <stdio.h>
+#include <string.h>
 
-int main( int argc, char *argv[] )  {
+int main( int argc, char *argv[] )
+{
+const char wav_check[5] = ".wav";
+const char txt_check[5] = ".txt";
+char *wav_ret;
+char *txt_ret;
+wav_ret = strstr(argv[1], wav_check);
+txt_ret = strstr(argv[2], txt_check);
 
-   if( argc == 2 ) {
-      printf("Continue\n");
-      
-      
-   }
-   else if( argc > 2 ) {
-      printf("ERROR: insufficient arguments. Enter main.c 'file.wav' 'target_data.txt'\n");
+   if( argc == 3 ) {
+        if ((wav_ret = ".wav") && (txt_ret = ".txt")) {
+            printf("Continue\n");
+        
+            
+            
+            
+            
+            
+            
+        }
+        else {
+            printf("ERROR: Please enter only .wav and or .txt files");
+        }
    }
    else {
-      printf("ERROR: insufficient arguments. Enter main.c 'file.wav' 'target_data.txt'\n");
+      printf("ERROR: insufficient arguments. Enter main.c 'example_file.wav' 'example_data.txt'\n");
    }
+   
+   return (0);
 }
